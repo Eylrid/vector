2c6f25a25b1594f2fbbbcd0f2001aed35649caf6
Amos Bairn <Eylrid@Gmail.com>
2015-04-09 09:31:33 -0700

    Make Vector a subclass of tuple
    This has two desirable outcomes:
    
    1. It makes vectors immutable
    
    2. vector components can be gotten directly with things like vector[4] and
    vector[2:6:2]

--- vector.py
+++ vector.py
@@ -1,38 +1,38 @@
 import math
 
-class Vector:
-    def __init__(self, components):
-        self.components = components
+class Vector(tuple):
+    def __new__(cls, components):
+        return tuple.__new__(cls, components)
 
     def __repr__(self):
-        return '<' + ', '.join([str(i) for i in self.components]) + '>'
+        return '<' + ', '.join([str(i) for i in self]) + '>'
 
     def __add__(self, other):
         '''Adds self and other and returns a new vector'''
         self.check_other(other, '+')
-        components = [a+b for a,b in zip(self.components, other.components)]
+        components = [a+b for a,b in zip(self, other)]
         return Vector(components)
 
     def __sub__(self, other):
         '''Subtracts other from self and returns a new vector'''
         self.check_other(other, '-')
-        components = [a-b for a,b in zip(self.components, other.components)]
+        components = [a-b for a,b in zip(self, other)]
         return Vector(components)
 
     def __mul__(self, scalar):
         '''Multiples the vector by scalar and returns a new vector'''
-        components = [a*scalar for a in self.components]
+        components = [a*scalar for a in self]
         return Vector(components)
 
     def __div__(self, scalar):
         '''Divides the vector by scalar and returns a new vector'''
-        components = [a/scalar for a in self.components]
+        components = [a/scalar for a in self]
         return Vector(components)
 
     def dot(self, other):
         '''Return the dot product of self and other'''
         self.check_other(other, 'dot')
-        return sum([a*b for a,b in zip(self.components, other.components)])
+        return sum([a*b for a,b in zip(self, other)])
 
     def orthogonal(self, other):
         '''Return True if self and other are orthogonal'''
@@ -58,14 +58,14 @@ class Vector:
 
     def dimensions(self):
         '''Returns the number of dimensions the vector is defined for'''
-        return len(self.components)
+        return len(self)
 
     def length(self):
         '''Returns the length of the vector'''
-        return math.sqrt(sum([i**2 for i in self.components]))
+        return math.sqrt(sum([i**2 for i in self]))
 
     def direction(self):
         '''Returns a new unit vector with the same direction as self'''
         length = self.length()
-        components = [i/length for i in self.components]
+        components = [i/length for i in self]
         return Vector(components)
